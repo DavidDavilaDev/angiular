ede06362c47d26ac9d9389e963d342da79c83b65
Guayaba15 <daviddavilabarrios@gmail.com>
2024-06-27 01:00:54 -0600

    btn logout created

--- src/app/view/side-nav/side-nav.component.ts
+++ src/app/view/side-nav/side-nav.component.ts
@@ -3,6 +3,9 @@ import { MatSidenav } from '@angular/material/sidenav';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/service/user.service.service';
+
 
 @Component({
   selector: 'app-side-nav',
@@ -13,11 +16,20 @@ export class SideNavComponent {
   @ViewChild('sidenav') sidenav!: MatSidenav;
   isHandset: boolean = false;
 
-  constructor(private breakpointObserver: BreakpointObserver) {
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private router: Router,
+    private userService: UserService
+  ) {
     this.breakpointObserver.observe([Breakpoints.Handset])
       .pipe(
         map(result => result.matches),
         shareReplay()
       ).subscribe(isHandset => this.isHandset = isHandset);
+  }
+
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['/']);
   }
 }
